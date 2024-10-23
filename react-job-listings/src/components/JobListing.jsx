dddee75f3d67496bcee26912ee6ad37fb3509063
Alex Bae <alexs39bae@gmail.com>
2024-10-23 04:18:07 -0700

    added react icons to job listing

--- react-job-listings/src/components/JobListing.jsx
+++ react-job-listings/src/components/JobListing.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from 'react';
+import { FaMapMarker } from 'react-icons/fa'
 
 const JobListing = ({ job }) => {
   const [showFullDescription, setShowFullDescription] = useState(false);
@@ -21,7 +22,8 @@ const JobListing = ({ job }) => {
 
         <div className="mb-5">{description}</div>
 
-        <button className="text-indigo-599 mb-5 hover:text-indigo-600">
+        <button onClick={() => setShowFullDescription((prevState) =>
+        !prevState)} className="text-indigo-599 mb-5 hover:text-indigo-600">
         { showFullDescription ? 'Less' : 'More' }
         </button>
 
@@ -31,7 +33,7 @@ const JobListing = ({ job }) => {
 
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="text-orange-700 mb-3">
-            <i className="fa-solid fa-location-dot text-lg"></i>
+            <FaMapMarker className='inline text-lg mb-1 mr-1'/>
             {job.location}
           </div>
           <a
