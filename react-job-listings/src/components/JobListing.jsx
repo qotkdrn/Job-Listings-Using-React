804ae4dafca31d1693d5f77fb0b87a3715ab6f52
Alex Bae <alexs39bae@gmail.com>
2024-10-23 04:10:27 -0700

    added view all jobs component

--- react-job-listings/src/components/JobListing.jsx
+++ react-job-listings/src/components/JobListing.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import { useState } from 'react';
 
 const JobListing = ({ job }) => {
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
+  let description = job.description;
+
+  if(!showFullDescription) {
+    description = description.substring(0, 90) + '...';
+
+  }
+
   return (
     <div className="bg-white rounded-xl shadow-md relative">
       <div className="p-4">
@@ -8,7 +18,13 @@ const JobListing = ({ job }) => {
           <div className="text-gray-600 my-2">{job.type}</div>
           <h3 className="text-xl font-bold">{job.title}</h3>
         </div>
-        <div className="mb-5">{job.description}</div>
+
+        <div className="mb-5">{description}</div>
+
+        <button className="text-indigo-599 mb-5 hover:text-indigo-600">
+        { showFullDescription ? 'Less' : 'More' }
+        </button>
+
         <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
 
         <div className="border border-gray-100 mb-5"></div>
