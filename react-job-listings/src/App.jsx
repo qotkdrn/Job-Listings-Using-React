b0a83ff81f68079237704a1d6fd9af8659df2b54
Alex Bae <alexs39bae@gmail.com>
2024-10-23 18:15:37 -0700

    made some changes to app, navbar, homecards to use link from react-router-dom

--- react-job-listings/src/App.jsx
+++ react-job-listings/src/App.jsx
@@ -14,7 +14,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
-      <Route path='/jobs' element={<Page />} />
+      <Route path='/jobs' element={<JobsPage />} />
     </Route>
   )
 )
