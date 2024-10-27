15c7d5542a695d0cd3c826a7ae892935783df9c2
Alex Bae <alexs39bae@gmail.com>
2024-10-27 11:57:58 -0700

    added toastify

--- react-job-listings/src/pages/JobPage.jsx
+++ react-job-listings/src/pages/JobPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams, useLoaderData, useNavigate } from "react-router-dom";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { toast } from 'react-toastify';
 
 
 const JobPage = ({ deleteJob }) => {
@@ -15,6 +16,8 @@ const JobPage = ({ deleteJob }) => {
     if(!confirm) return;
 
     deleteJob(jobId);
+
+    toast.success('Job deleted successfully');
 
     navigate('/jobs');
   }
