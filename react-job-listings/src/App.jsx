60074335a5d9db59efe4025e28fed5b828efc781
Alex Bae <alexs39bae@gmail.com>
2024-10-27 12:33:17 -0700

    added edit job page

--- react-job-listings/src/App.jsx
+++ react-job-listings/src/App.jsx
@@ -12,6 +12,7 @@ import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
 import JobPage, { jobLoader } from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
+import EditJobPage from "./pages/EditJobPage";
 
 const addJob = async (newJob) => {
   //Add New Job
@@ -33,6 +34,18 @@ const deleteJob = async (id) => {
   return;
 }
 
+//update Job
+const updateJob = async (job) => {
+  const res = await fetch(`/api/jobs/${job.id}`,{
+    method: 'PUT', 
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(job),
+  })
+  return;
+}
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
@@ -40,6 +53,7 @@ const router = createBrowserRouter(
       <Route path='/jobs' element={<JobsPage />} />
       <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
       <Route path='/jobs/:id' element={<JobPage deleteJob={ deleteJob }/>} loader={jobLoader}/>
+      <Route path='/edit-job/:id' element={<EditJobPage updateJobSubmit={updateJob}/>} loader={jobLoader}/>
       <Route path='/*' element={<NotFoundPage />} />
     </Route>
   )
